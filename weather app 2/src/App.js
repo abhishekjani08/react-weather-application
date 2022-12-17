d17bf307da99bba08e3624bc90364e10a83373fe
50_Abhishek Jani <88500027+abhishekjani08@users.noreply.github.com>
2022-12-17 21:43:20 +0530

    main

--- weather app 2/src/App.js
+++ weather app 2/src/App.js
@@ -1,20 +1,12 @@
 import './App.css';
 import Weatherapp from './components/Weatherapp';
 function App() {
-  const mystyle = {
-    color: "white",
-    backgroundColor: "DodgerBlue",
-    padding: "10px",
-    fontFamily: "Montserrat",
-    display: "flex",
-    justifycontent: "center",
-    alignitems: "center"
-  };
 
   return (   
     <div>
       <h1 className='mystyle'> Weather App</h1>
       <Weatherapp/>
+      <footer/>
     </div>
   );
 }
--- weather app 2/src/components/Weatherapp.js
+++ weather app 2/src/components/Weatherapp.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./css/style.css";
 import { useState } from "react";
 const Weatherapp = () => {
 
-    const [city,setCity]=useState("Mumbai");
+    const [city, setCity] = useState(null);
+    const [search, setSearch] = useState("Mumbai");
+
+    useEffect(() => {
+        const fetchApi = async () => {
+            const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=896f2a1fe32e5f466140d9af63dd2a0f`;
+            const response = await fetch(url);
+            // console.log(response)
+            const resJson = await response.json();
+            console.log(resJson);
+            setCity(resJson.main);
+        }
+
+        fetchApi();
+
+    }, [search])
 
     return (
         <>
@@ -12,42 +27,42 @@ const Weatherapp = () => {
                     <input type="search"
                         className="inputfield"
                         defaultValue=""
+                        value={search}
                         onChange={(event) => {
-
+                            setSearch(event.target.value)
                         }}
                     />
                     <span className="search">
-                        <i class="fa fa-search" aria-hidden="true"></i>
+                        <i className="fa fa-search" aria-hidden="true"></i>
                     </span>
                 </div>
 
-                <div className="info">
-                    <div className="location">
-                        <i class="fa fa-map-marker" aria-hidden="true"></i>  {city}
-                    </div>
-                    <div className="temp">
-                        15deg
-                    </div>
-                    <div className="minmaxtemp">
-                        15min-20max
-                    </div>
-                </div>
 
+                {city ? (
+                    <>
+                        <div className="info">
+                            <div className="location">
+                                <i className="fa fa-map-marker" aria-hidden="true"></i>  {search}
+                            </div>
+                            <div className="temp">
+                               Temperature at Present : {city.temp}°C
+                            </div>
 
-{/* 
-                    <ul class="circles">
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                    </ul> */}
+                            <div className="minmaxtemp">
+                                Min-Max Temperature:{city.temp_min}°C-{city.temp_max}°C
+                            </div>
+                        </div>
+                    </>
+                )
+                    :
+                    (
+                        <p className="nocity"><h2>No City named with {search} </h2></p>
+                    )
+
+                }
             </div>
+            <h4 className='footer'>Designed by Abhishek Jani</h4>
+
         </>
     )
 }
