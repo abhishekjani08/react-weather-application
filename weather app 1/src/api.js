33992c1d217764be89aba165e36b33cebe70454d
50_Abhishek Jani <88500027+abhishekjani08@users.noreply.github.com>
2022-12-17 21:50:39 +0530

    main

--- weather app 1/src/api.js
+++
@@ -1,9 +0,0 @@
-export const geoApiOptions = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '4fcebe3237msh1d91e4981880439p17b143jsn370274fabdb7',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-	}
-};
-
-export const GEO_API_URL="https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
